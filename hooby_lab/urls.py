bf6e7f79d4be2c15dd6f164b99ed9da9cea58cdd
petermirithu <pyra_m.k@yahoo.com>
2020-01-20 13:43:09 +0300

    include registration urls and all auth urls

--- hooby_lab/urls.py
+++ hooby_lab/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('hooby_app.urls')),    
+    url(r'^accounts/',include('registration.backends.simple.urls')),
+    url(r'^accounts/',include('allauth.urls'))
 ]
