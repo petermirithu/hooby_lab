d16e343008ef2a5f56cd82382859a86ef12dfcba
petermirithu <pyra_m.k@yahoo.com>
2020-01-20 13:39:59 +0300

    add a profile model

--- hooby_app/models.py
+++ hooby_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from pyuploadcare.dj.models import ImageField
+class profile(models.Model):
+  '''
+  class that defines how a users profile shall look like
+  '''
+  user=models.OneToOneField(User,on_delete=models.CASCADE)
+  profile_pic=ImageField(blank=True,manual_crop='')
+  status=models.CharField(max_length=100)
 
-# Create your models here.
+  # def __str__(self):
+  #     return self.user.username`
+  
+
