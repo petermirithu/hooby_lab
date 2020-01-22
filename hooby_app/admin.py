b9c78c00815cfcd856655a1e16fc8a5f1a1c2cbc
petermirithu <pyra_m.k@yahoo.com>
2020-01-23 00:46:41 +0300

    register room model

--- hooby_app/admin.py
+++ hooby_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import profile,reviews
+from .models import profile,reviews,room
 
 # Register your models here.
 admin.site.register(profile)
 admin.site.register(reviews)
+admin.site.register(room)
