9db74ad89bbe9cc5c6eb22c321d647d3c2fcad59
petermirithu <pyra_m.k@yahoo.com>
2020-01-22 10:43:23 +0300

    add a reviews model

--- hooby_app/models.py
+++ hooby_app/models.py
@@ -13,3 +13,17 @@ class profile(models.Model):
   #     return self.user.username`
   
 
+class reviews(models.Model):
+  music_id=models.CharField(max_length=20)
+  body=models.CharField(max_length=1000)
+  posted_by=models.ForeignKey(User, on_delete=models.CASCADE)
+  posted_on=models.DateField(auto_now_add=True)
+
+  def __str__(self):
+    return self.body
+
+  @classmethod
+  def get_reviews(cls,music_id):
+    reviews=cls.objects.filter(music_id__icontains=music_id)
+    return reviews
+  
