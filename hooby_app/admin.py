aefd690dfe847cc03033056ee4efa09e64862b3d
petermirithu <pyra_m.k@yahoo.com>
2020-01-22 10:41:34 +0300

    register reviews model

--- hooby_app/admin.py
+++ hooby_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import profile
+from .models import profile,reviews
 
 # Register your models here.
 admin.site.register(profile)
+admin.site.register(reviews)
